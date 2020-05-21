c69caea77920623cf1dbc13ad1a70038d1fbd4ee
Marc Pahnke <marc.pahnke@gmx.de>
2020-05-21 22:49:47 +0200

    First public release [~55.5 % done] please be patient... https://github.com/openlsus/lsus/milestone/1

--- bin/lsusversion.py
+++ bin/lsusversion.py
@@ -3,8 +3,8 @@
 from __future__ import print_function
 
 import sys
-
-
+from time import gmtime, strftime
+CURRENTYEAR=strftime("%Y", gmtime())
 
 VMAJOR=0
 VMINOR=11
@@ -20,11 +20,14 @@ FULL_PRODUCT=VENDOR + ' ' + ALIAS + ' ' + PRODUCT
 VERSION=str( VMAJOR) + '.' + str( VMINOR )
 
 COMPLETE = FULL_PRODUCT + ' ' + VERSION
+COPYRIGHT='2015-' + CURRENTYEAR
+COPYRIGHTTEXT='Copyright(c) ' + COPYRIGHT + ' by marc.pahnke@gmx.de'
 
 _V_ = '-v'
 _N_ = '-n'
 _C_ = '-c'
 _U_ = '-u'
+_L_ = '-l'
 __V = '--v'
 _h_ = "-h"
 __help = "--help"
@@ -32,6 +35,9 @@ __help = "--help"
 HELP = '\nHelp for Parameters\n\t' + _V_ + ' for verbose' + '\n\t' + _N_ + ' for no linefeed' 
 HELP += '\n\t' + __V + ' filename_compliant_format_v.2' + '\n\t' + _U_ + ' pre tar apply_update_transformation_a_b_format ' 
 HELP += '\n\t' + _C_ + ' no linefeed plus client pack version' +  '\n\t-h/--help this help message' 
+
+def getCopyright():
+    return COPYRIGHTTEXT
 
 if __name__ == '__main__':
     
@@ -50,6 +56,9 @@ if __name__ == '__main__':
       
     elif len( sys.argv ) > 1 and sys.argv[ 1 ] == _C_:
       print( VERSION + '.' + str( CLIENT_PACK_VERSION ), end='')
+
+    elif len( sys.argv ) > 1 and sys.argv[ 1 ] == _L_:
+      print( COPYRIGHTTEXT) #, end='')
       
     elif len( sys.argv ) > 1 and ( sys.argv[ 1 ] == _h_ or sys.argv[ 1 ] == __help ):
       print( FULL_PRODUCT, VERSION, HELP )
