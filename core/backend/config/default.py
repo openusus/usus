26741cbd947c7e5fa7a7fc8576856ef8cca216cd
Marc Pahnke <marc.pahnke@gmx.de>
2022-05-05 21:19:04 +0200

    #8 Added new Config.py as general compat config access

---
+++ core/backend/config/default.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+import os
+
+CFGDIRNAME='conf'
+HOSTS = """Hosts"""
+
+CFGFILENAME='lsus.cfg'
+
+# LOG DB COLUMNS
+LOG       = 'LOG'
+IPADDRESS = "Ipaddress"
+USERID    = "Userid"
+USERNAME  = "Username"
+TIMESTAMP = "Timestamp"
+ACTION    = "Action"
+STATUS    = "Status"
+DATA      = "Data"
+
+
+UNKNOWN_LSUS_USER = 'Unknown_LSUS_User'
+
+# DB.Core returns the config-Dict
+# reads the ConfigFile which returned by getConfigFilePath()
+# and assign its Fields to
+# result[CFGKEYDBHOST]
+# result[CFGKEYDBNAME]
+# result[CFGKEYDBUSER]
+# result[CFGKEYDBPASS]
+# todo: replace error-out with write2errorlogfile && "error occurred (((reading config)))?, check logfile"
+
+# Core.Config.Get.DataFolder
+#  todo: write and use builtin nosql
+#  todo: add some file(xyz.py). exists checks before return.
+#    - check for symlink spoofing cross-site injections
+def getBaseDir():
+  # __file__ == core/backend/config/default.py
+  configpath = os.path.dirname( os.path.realpath( __file__ ) )
+  return os.path.realpath( os.path.join( configpath, "..", "..", ".." ) )
+
+# Core.Config.Get.ConfDir
+# Gets the default config dir
+# todo. clarify - cleanup, deduplicate redundant code, use builtin nosql
+def getConfDir():
+  # binpath=os.path.dirname( os.path.realpath( __file__ ) )
+  return os.path.realpath( os.path.join( getBaseDir(), CFGDIRNAME ) )
+
+
+HOSTS_TABLE = 'Hosts'
